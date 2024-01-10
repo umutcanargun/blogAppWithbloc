167c7fcc9340b0df2722a5262852655993737293
UmutcanArgun <36049614+umutcanargun@users.noreply.github.com>
2024-01-10 13:14:22 +0300

    Update blog_detail.dart

--- BlogApp/miniblogApWithBloc/lib/screens/blog_detail.dart
+++ BlogApp/miniblogApWithBloc/lib/screens/blog_detail.dart
@@ -22,46 +22,14 @@ class _BlogDetailState extends State<BlogDetail> {
   @override
   void initState() {
     super.initState();
-    //fetchBlog();
+   
   }
 
-  /*fetchBlog() async {
-    Uri url = Uri.parse(
-        "https://tobetoapi.halitkalayci.com/api/Articles/${widget.id}");
-    final response = await http.get(url);
-    final jsonData = json.decode(response.body);
-
-    setState(() {
-      blog = Blog.fromJson(jsonData);
-    });
-  }*/
+ 
 
   @override
   Widget build(BuildContext context) {
-    /*return Scaffold(
-      appBar: AppBar(
-        title: Text(blog?.title ?? ""),
-      ),
-      body: blog == null
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.network(blog?.thumbnail ?? ""),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      children: [
-                        Text(blog?.title ?? ""),
-                        Text(blog?.content ?? ""),
-                        Text(blog?.author ?? ""),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-    );*/
+
 
     return BlocBuilder<DetailBloc, DetailState>(
                 builder: (context, state) {
